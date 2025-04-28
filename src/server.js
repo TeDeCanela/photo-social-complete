598f8733db79fbe133a5150804aac5f347a470db
TeDeCanela <153479319+TeDeCanela@users.noreply.github.com>
2025-04-28 12:00:29 -0600

    Primer commit

---
+++ src/server.js
@@ -0,0 +1,27 @@
+require("dotenv").config({ path: require("path").resolve(__dirname, "../.env") });
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+app.use("/uploads", express.static("uploads")); 
+app.use("/docs/test-coverage", express.static("../coverage/lcov-report"))
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB conectado"))
+  .catch((err) => console.error("Error de conexión:", err));
+
+const photoRoutes = require("./routes/photoRoutes");
+app.use("/api/photos", photoRoutes);
+
+module.exports = app; 
+
+if (require.main === module) {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+}
